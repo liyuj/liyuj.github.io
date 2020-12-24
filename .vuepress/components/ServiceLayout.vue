65bb030fc54b54b878a9ae14051d0130260bdcc7
liyujue <18624049226@163.com>
2020-12-24 12:01:05 +0800

    服务细节调整

--- .vuepress/components/ServiceLayout.vue
+++ .vuepress/components/ServiceLayout.vue
@@ -31,7 +31,7 @@
 				<!-- /COLUMN -->
 				<!-- COLUMN -->
 				<div class="col-sm-9 text-left scroll-animated-from-bottom" style="padding-top:10px;border-left:4px dashed #777">
-          <h4><span>服务内容：</span><span style="font-weight:normal">初始技术调查，Ignite专题介绍/远程培训，协助POC/技术架构咨询/规划/评审，协助解决具体的测试/生产问题、系统性能/扩展性优化等，GridGain基础版服务（社区版版本更新），GridGain标准版服务（社区版版本更新+紧急修复补丁）</span></h4>
+          <h4><span>服务内容：</span><span style="font-weight:normal">初始技术调查、Ignite专题介绍/远程培训、协助POC/业务原型开发/Ignite定制化（驻场），基于Ignite的技术集成架构设计咨询/规划/评审、协助解决具体的测试/生产问题（远程服务需配合提供完整日志等必要的信息或接入技术手段）、系统性能/扩展性优化等，GridGain基础版服务（社区版版本更新），GridGain标准版服务（社区版版本更新+紧急修复补丁）</span></h4>
           <h4><span>服务方式：</span><span style="font-weight:normal">中国服务商实施（远程/驻场），GridGain远程（基础版/标准版服务）</span></h4>
           <h4><span>服务报价：</span><span style="font-weight:normal">按年度订阅，GridGain服务包（按GCU计费，全款预付）+ 中国服务商服务包（标准远程服务10万元，驻场服务3000元/核算日，可分期付款）</span></h4>
 				</div>
@@ -43,16 +43,16 @@
 			<div class="client-feedback-item row">
 				<!-- COLUMN -->
 				<div class="col-sm-3 text-center scroll-animated-from-bottom"  style="color:red">
-          <span class="fa fa-battery-3" style="font-size:36px;margin:10px 0 10px 0"></span>
+          <span class="fa fa-battery-3" style="font-size:36px;margin:20px 0 10px 0"></span>
 					<h3 style="font-size:18px;margin:0 0 10px 0">技术咨询顾问服务</h3>
           <h4>面向企业</h4>
 				</div>
 				<!-- /COLUMN -->
 				<!-- COLUMN -->
 				<div class="col-sm-9 text-left scroll-animated-from-bottom" style="padding-top:10px;border-left:3px dashed #777">
-					<h4><span>服务内容：</span><span style="font-weight:normal">初始技术调查，Ignite专题介绍/远程培训，协助POC/业务原型开发/Ignite定制化（驻场）、技术架构咨询/规划/评审，协助解决具体的测试/生产问题、Ignite问题确认并提供折衷方案、系统性能/扩展性优化等。</span></h4>
+					<h4><span>服务内容：</span><span style="font-weight:normal">初始技术调查、Ignite专题介绍/远程培训、协助POC/业务原型开发/Ignite定制化（驻场）、基于Ignite的技术集成架构设计咨询/规划/评审、协助解决具体的测试/生产问题（远程服务需配合提供完整日志等必要的信息或接入技术手段）、Ignite问题确认并提供折衷方案、系统性能/扩展性优化、基于开源代码库维护Ignite版本等。</span></h4>
           <h4><span>服务方式：</span><span style="font-weight:normal">远程（远程接入/网络会议/电话/即时通信软件/邮件）/驻场</span></h4>
-          <h4><span>服务报价：</span><span style="font-weight:normal">远程（6千元/半月，1万元/月，2.5万元/季，5万元/半年，10万元/年），驻场（3000元/核算日，4.5万元/月，长期合作面议）</span></h4>
+          <h4><span>服务报价：</span><span style="font-weight:normal">远程（4千元/周，6千元/半月，1万元/月，2.5万元/季，5万元/半年，10万元/年），驻场（3000元/核算日，4.5万元/月，长期合作面议）</span></h4>
 				</div>
 				<!-- /COLUMN -->
 			</div>
@@ -69,7 +69,7 @@
 				<!-- /COLUMN -->
 				<!-- COLUMN -->
 				<div class="col-sm-9 text-left scroll-animated-from-bottom" style="padding-top:10px;border-left:2px dashed #777">
-					<h4><span>服务内容：</span><span style="font-weight:normal">初始技术调查，Ignite功能讲解/答疑，指导POC/应用开发，指导解决具体的测试/生产问题（需配合提供完整日志等必要的信息）、Ignite问题确认并提供折衷方案、系统性能/扩展性优化等。</span></h4>
+					<h4><span>服务内容：</span><span style="font-weight:normal">初始技术调查、Ignite功能讲解/答疑、指导POC/应用开发、基于Ignite的技术集成架构设计咨询、指导解决具体的测试/生产问题（需配合提供完整日志等必要的信息）、Ignite问题确认并提供折衷方案、系统性能/扩展性优化等。</span></h4>
           <h4><span>服务方式：</span><span style="font-weight:normal">语音（优先）/即时通信软件/电子邮件</span></h4>
           <h4><span>服务时间：</span><span style="font-weight:normal">8:30～21:30</span></h4>
           <h4><span>服务报价：</span><span style="font-weight:normal">200元/半月/人，300元/月/人，600元/季/人，1000元/半年/人，1800元/年/人</span></h4>
